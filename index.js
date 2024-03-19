a804431cf67bcb630804234bd19bdc10741dd0ac
AUDREY BAUDRY <baudry.audrey@outlook.fr>
2024-03-19 11:29:00 +0100

    pour le push

---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+const log = console.log 
+log(chalk.blue.bgYellow.bold('Arnaud'));
+log(chalk.red.bgGreen.dim('Alexis'));
+log(chalk.black.bgYellow.italic('Audrey'));
+log(chalk.greenBright.bgMagenta.bold('Romain'));
+
